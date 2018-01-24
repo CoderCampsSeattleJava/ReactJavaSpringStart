bdda1c03a424b014837151f04df8a39a7536d283
Unknown <iglumu@gmail.com>
2018-01-24 11:26:55 -0800

    Created index.js contacts.js contactitem.js

---
+++ my-app/public/ContactItem.js
@@ -0,0 +1,14 @@
+var ContactItem = React.createClass({
+    propTypes: {
+        name: React.PropTypes.string.isRequired,
+    },
+
+    render: function () {
+        return(
+            React.createElement('li', {className: 'Contact'},
+        React.createElement('h2', { className: 'Contact-name' },
+        trhis.props.name)
+            )
+        )
+    },
+});
