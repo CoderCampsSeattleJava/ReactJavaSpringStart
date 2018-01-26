3c2240dd390bd2cdf3b581ddd6fb57486b73a24f
Unknown <iglumu@gmail.com>
2018-01-26 11:54:40 -0800

    initial commit

---
+++ Java/RemoteSystemsTempFiles/src/main/java/com/ReactJavaMySQL/Startup/MainController.java
@@ -0,0 +1,44 @@
+package com.ReactJavaMySQL.Startup;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.ReactJavaMySQL.Startup.User;
+import com.ReactJavaMySQL.Startup.UserRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@Controller
+@RequestMapping(path="/api")
+public class MainController {
+	
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping(path="/add")
+	public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
+		
+		User n = new User();
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "saved";
+	}
+	
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+	
+}
