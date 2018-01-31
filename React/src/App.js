670058298c8758a32b367be6839ff11139415ec8
Unknown <iglumu@gmail.com>
2018-01-31 10:39:50 -0800

    Added Github card feature and installed AXIOS

--- React/src/App.js
+++ React/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 const url = 'http://localhost:8080/api/all.json';
 
@@ -38,4 +39,83 @@ class App extends Component {
  }
 }
 
-export default App;
+
+const Card = props => {
+  return (
+    <div style={{ margin: "1em" }}>
+      <img alt="avatar" style={{ width: "70px" }} src={props.avatar_url} />
+      <div>
+        <div style={{ fontWeight: "bold" }}>
+          {props.name}
+        </div>
+        <div>
+          {props.blog}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CardList = props => {
+  return (
+    <div>
+      {props.cards.map(card => <Card {...card} />)}
+    </div>
+  );
+};
+
+class Form extends React.Component {
+  state = {
+    userName: ""
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}`)
+      .then(resp => {
+        this.props.onSubmit(resp.data);
+        this.setState({ userName: "" });
+      });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          placeholder="Add your Github "
+          required
+        />
+        <button type="submit">Add it now</button>
+      </form>
+    );
+  }
+}
+
+class App1 extends React.Component {
+  state = {
+    cards: []
+  };
+
+  addNewCard = cardInfo => {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.addNewCard} />
+        <CardList cards={this.state.cards} />
+      </div>
+    );
+  }
+}
+
+
+export default App1;
