9cf54e3730e5fa2b4a7139579205dbafeaeb56e8
Unknown <iglumu@gmail.com>
2018-02-12 10:40:54 -0800

    Commit 1040am

--- React/src/index.js
+++ React/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Register from './containers/Register';
 import { render } from 'react-dom';
-import 'react-bootstrap';
-import './styles.css';
-import {  HomePage, Dashboard } from './screens';
+import ReactBootstrap from 'react-bootstrap';
+// import './styles.css';
+import {  Dashboard } from './screens';
 import {  BrowserRouter as Router, Route } from 'react-router-dom';
 
  
 const Router1 = () => (
     <Router>
     <div>
-        <Route path="/HomePage" component={HomePage} />
+        {/* <Route path="/HomePage" component={HomePage} /> */}
         <Route path="/Dashboard" component={Dashboard} />
     </div>
     </Router>
