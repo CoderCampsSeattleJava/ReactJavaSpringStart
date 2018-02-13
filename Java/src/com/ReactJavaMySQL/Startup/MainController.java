418f817419c6ec49340fa2bf52e16b18e2db9ae5
Unknown <iglumu@gmail.com>
2018-02-13 08:58:02 -0800

    BackEnd Default Setup

--- Java/src/com/ReactJavaMySQL/Startup/MainController.java
+++ Java/src/com/ReactJavaMySQL/Startup/MainController.java
@@ -1,16 +1,5 @@
 package com.ReactJavaMySQL.Startup;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.ReactJavaMySQL.Startup.User;
-import com.ReactJavaMySQL.Startup.UserRepository;
-
 
 @CrossOrigin(origins = "http://localhost:3000")
 @Controller
