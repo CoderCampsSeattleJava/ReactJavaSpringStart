980bfaa1700ff97770e0de52191eae965ceac395
walaeJ <waljesusme2013@gmail.com>
2018-02-14 13:11:49 -0800

    solving

---
+++ React/src/App.js
@@ -0,0 +1,20 @@
+// 
+import React, { Component } from 'react';
+import axios from "axios";
+import Register from './containers/Register';
+
+
+class App extends React.Component {
+
+ 
+ render() {
+   return (
+     <div>
+       <Register />
+
+     </div>
+   );
+ }
+}
+
+export default Register;
--- React/src/index.js
+++ React/src/index.js
@@ -1,13 +1,73 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import './styles.css';
+//import App from './App';
+//import HomePage from './screens/HomePage';
+//import Dashboard  from './screens/Dashboard';
+
+
+
+        // import  { BrowserRouter as Router, Route }  from 'react-router-dom';
+         // //import { BrowserRouter as Router, Router, Link } from 'react-router-dom'
+
+
+
+               // const Router1 = () => (
+            //    <Router>
+              //     <div>
+            //       <Route path="/HomePage" component={HomePage} />
+              //         <Route path="/Dashboard" component={Dashboard} />
+           //         <Route path='/Register' component={Register}/>
+           //     </div>
+          //     </Router>
+         //  );
+ 
+ 
+                   //render(
+                  // <Router1 />, document.getElementById('root')
+                  //);
+                 //render(
+                      //<Router1 />, domcument.getElementById('root')
+                    //);
+//ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(  <Dashboard />, document.getElementById('root')
+//);
+
+//ReactDOM.render(
+  //<HomePage  />,    document.getElementById('root')  
+//);
+
+
+// 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HomePage from './screens/HomePage';
+import App from './App';
+import Register from './containers/Register';
+import { render } from 'react-dom';
+import ReactBootstrap from 'react-bootstrap';
 import './styles.css';
+import { Navbar, Nav, MenuItem, NavDropdown, NavItem, PageHeader, Grid, Row, Col, ListGroup, ListGroupItem, Jumbotron, Button } from 'react-bootstrap';
+
+import {  Dashboard } from './screens';
+import {  BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './screens/HomePage';
 
 
-ReactDOM.render(
-    <HomePage />,
-    document.getElementById('root')  
+const Router1 = () => (
+   <Router>
+   <div>
+       <Route path="/HomePage" component={HomePage} />
+       <Route path="/Dashboard" component={Dashboard} />
+       <Route path='/Register' component={Register}/>
+   </div>
+   </Router>
 );
+
+
+render(
+   <Router1 />, document.getElementById('root')
+);
+
 
 
 
