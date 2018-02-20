a55a478052d26dd6075f47a952161340dc4f13c9
walaeJ <waljesusme2013@gmail.com>
2018-02-20 12:52:51 -0800

    Merged from latest master

--- React/src/index.js
+++ React/src/index.js
@@ -5,7 +5,7 @@ import Register from './containers/Register';
 import ReactBootstrap from 'react-bootstrap';
 import './styles.css';
 import { Navbar, Nav, MenuItem, NavDropdown, NavItem, PageHeader, Grid, Row, Col, ListGroup, ListGroupItem, Jumbotron, Button } from 'react-bootstrap'
-import { HomePage, Dashboard, Register, Videos } from './screens';
+import { HomePage, Dashboard } from './screens';
 import {  BrowserRouter as Router, Route } from 'react-router-dom';
 import Videos from './containers/Videos';
 import Weather from './containers/weather';
