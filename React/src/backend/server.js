752daed121ee8e87fb470b629b39d988edb2cad0
Tyler Fuqua <tyler@agilityda.com>
2018-02-21 17:12:41 -0700

    server.js done

--- React/src/backend/server.js
+++ React/src/backend/server.js
@@ -2,5 +2,26 @@ const express = require('express');
 const login = require('./routes/LoginRoutes');
 const bodyParser = require('body-parser');
 
-const Router1 = express();
+const app = express();
 
+app.use( bodyParser.urlencoded ({ extended: true }));
+app.use( bodyParser.json());
+
+Router1.use(( req, res, next ) => {
+    res.header("Access-Control-Allow-Origin", " * ");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const router = express.Router();
+
+// Test Route
+router.get('/', ( req, res ) => {
+    res.json({ message: "Welcome to our upload module apis" });
+});
+
+// Route To Handle User Registration
+router.post('/register', login.register );
+router.post('/login', login.login );
+app.use('/api', router );
+app.listen( 3000 );
