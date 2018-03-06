2bcd7b9cb52dc90913454e00962ee44c43d522fc
Unknown <iglumu@gmail.com>
2018-03-06 14:04:34 -0800

    Fixed buttons and added button in Courses
    Fixed pages, COURSES and VIDEOS

--- React/src/screens/Courses.js
+++ React/src/screens/Courses.js
@@ -6,28 +6,22 @@ import Shake from 'react-reveal/Shake';
 import Center from 'react-center';
 import ResponsiveContainer from 'react-responsive-widget';
 import RubberBand from 'react-reveal/RubberBand';
-<<<<<<< HEAD
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-import { red100, blue900 } from 'material-ui/styles/colors';
+import { red100, blue900, blueA100, white100 } from 'material-ui/styles/colors';
 import { Button } from 'react-bootstrap';
-=======
 import { connect } from 'react-redux';
->>>>>>> master
 
 class Courses extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            course_name: '',
             course_id: '',
         };
-
 
         this.handleClick1 = this.handleClick1.bind(this);
         this.handleClick2 = this.handleClick2.bind(this);
         this.handleClick3 = this.handleClick3.bind(this);
-
     }
 
     handleClick1(e) {
@@ -43,22 +37,12 @@ class Courses extends React.Component {
         this.props.history.push('./Videos');
     }
 
-    // handleSubmit1(e) {
-    //     e.preventDefault();
-    //     console.log("Course name posted");
-    // }
-
-    // handleSubmit2(e) {
-    //     e.preventDefault();
-    //     console.log("Course ID posted");
-    // }
-
     onSubmit = (e) => {
         e.preventDefault();
         axios.post(`http://localhost:8080/api3/favourites?user_id=${this.props.userId}&course_id=${this.state.course_id}`)
             .then((response) => {
-                this.props.history.push('./Dashboard');
-                console.log(response, "Not Yet");
+                // this.props.history.push('./LoginPage');
+                console.log(response, "Saved to Favs");
             });
     }
 
@@ -70,38 +54,49 @@ class Courses extends React.Component {
             course2: 'HTML',
             course3: 'React.js',
         };
-        const { course_name, course_id } = this.state
+        const { course_id } = this.state
         let isEnabled =
-        course_id > 0; 
+            course_id > 0;
 
         return (
             <ResponsiveContainer><header>
                 <Center>
                     <div>
-                        <Center> <RubberBand>
-                            <div style={{ fontWeight: "bold", color: "white", fontSize: "xx-large" }}>
-                                Add COURSES to your FAVOURITES</div>
-                        </RubberBand> </Center>
+                        <Center>
+                            <RubberBand>
+                                <div style={{ fontWeight: "bold", color: "white", fontSize: "xx-large" }}>
+                                    Add COURSES to your FAVOURITES
+</div>
+                                <div>
+                                    <Button className='backtodash' bsStyle="primary" style={{ color: white100, fontWeight: "bold", fontSize: "large" }} onClick={this.handleClick1}>Back to Dashboard</Button>
+                                    <Button className='backtodash' bsStyle="primary" style={{ color: white100, fontWeight: "bold", fontSize: "large" }} onClick={this.handleClick2}>Back to Home</Button>
+                                    <Button className='backtodash' bsStyle="primary" style={{ color: white100, fontWeight: "bold", fontSize: "large" }} onClick={this.handleClick3}>Go to Videos</Button>
+                                </div>
+                            </RubberBand>
+                        </Center>
 
-                        <Center>  <Spin><Spin> <Spin><Spin><Shake> <Shake><Spin><Spin> <Spin><Spin><Spin><Spin><Spin><Spin><Shake><Shake><Spin><Spin><Spin><Spin>
+                        <Center><Spin><Spin> <Spin><Spin><Shake> <Shake><Spin><Spin> <Spin><Spin><Spin><Spin><Spin><Spin><Shake><Shake><Spin><Spin><Spin><Spin>
                             <img src={backgroundtest} width={250} height={250} />
                         </Spin></Spin></Spin></Spin></Shake></Shake></Spin></Spin></Spin></Spin></Spin></Spin></Spin></Spin></Shake></Shake></Spin></Spin></Spin></Spin></Center>
-                      <Center>  <select value={this.state.course_id}
-                            onChange={evt => this.setState({ course_id: evt.target.value })}
+                        <Center>
+                            <select size='3' multiple value={this.state.course_id}
+                                onChange={evt => this.setState({ course_id: evt.target.value })}
+                            >
+                                <option style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} value="1">Java</option>
+                                <option style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} value="2">HTML</option>
+                                <option style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} value="3">CSS</option>
+                                <option style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} value="4">EcmaScript JS</option>
+                                <option style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} value="5">React.js</option>
+                                <option style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} value="6">JQuery</option>
+                                <option style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} value="7">Bootstrap</option>
+                            </select>
 
-                        // onChange={this.handleSubmit1}
-                        >
-                            <option value="0">Select Course</option>
-                            <option value="1">Java</option>
-                            <option value="2">HTML</option>
-                            <option value="3">CSS</option>
-                            <option value="4">EcmaScript JS</option>
-                            <option value="5">React.js</option>
-                            <option value="6">JQuery</option>
-                            <option value="7 ">Bootstrap</option>
-                        </select>
-
-                        <RubberBand><RubberBand><button disabled={!isEnabled} onClick={e => this.onSubmit(e)}>Submit!</button></RubberBand></RubberBand></Center>
+                            <RubberBand>
+                                <RubberBand>
+                                    <Button style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} bsStyle="primary" disabled={!isEnabled} onClick={e => this.onSubmit(e)}>Submit!</Button>
+                                </RubberBand>
+                            </RubberBand>
+                        </Center>
                     </div>
                 </Center>
             </header>
--- React/src/screens/Videos.js
+++ React/src/screens/Videos.js
@@ -12,7 +12,7 @@ import linkedinicon from './images/linkedinicon.png';
 import instagramicon from './images/instagramicon.png';
 import youtubeicon from './images/youtubeicon.png';
 import axios from 'axios';
-
+import { red100, blue900, blueA100, white100 } from 'material-ui/styles/colors';
 class Videos extends Component {
     constructor() {
         super();
@@ -68,36 +68,20 @@ class Videos extends Component {
                     <div style={{ backgroundColor: "white" }}>
                         <Center><h2>Coder Dudes - Featured Videos</h2></Center></div>
 
-                    <Button bsStyle="primary" onClick={this.handleClick}>Back to Dashboard</Button>
+                    <Button style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} bsStyle="primary" onClick={this.handleClick}>Back to Dashboard</Button>
                     <Zoom><img src={backgroundtest} width={120} height={120} /></Zoom>
 
                     <Center> <Roll><h3>Coder's School of the Month</h3></Roll> </Center>
                     <Center>   <Wobble><iframe className='videopadding' title="CoderSchoolOfTheMonth" width="560" height="315" src="https://www.youtube.com/embed/V4w5R7_80G0"
                         frameBorder="50" allow="autoplay; encrypted-media" allowFullScreen></iframe><br /></Wobble></Center>
 
-                    {/* <select value={this.state.course_id} onChange={this.handleSubmit1}>
-                        <option value="N/A">Open Menu</option>
-                        <option value="000">Add to Favorites</option>
-                        <option value="000">LIKE video</option></select> */}
-
-
                     <Center>   <Roll left><h3>A day at Coder Camps</h3></Roll></Center>
                     <Center> <Wobble><iframe className='videopadding' title="ADayatCoderCamp" width="560" height="315" src="https://www.youtube.com/embed/bb6Gqa_hXV8"
                         frameBorder="50" allow="autoplay; encrypted-media" allowFullScreen></iframe><br /></Wobble> </Center>
 
-                    {/* <select value={this.state.course_id} onChange={this.handleSubmit2}>
-                        <option value="N/A">Open Menu</option>
-                        <option value="000">Add to Favorites</option>
-                        <option value="000">LIKE video</option></select> */}
-
                     <Center> <Roll><h3>Final Project of the Month</h3></Roll></Center>
                     <Center> <Wobble> <iframe className='videopadding' title="FInalProjectofTheMonth" width="560" height="315" src="https://www.youtube.com/embed/X_iQ5rgTadA"
                         frameBorder="50" allow="autoplay; encrypted-media" allowFullScreen></iframe><br /></Wobble></Center>
-
-                    {/* <select value={this.state.course_name} onChange={this.handleSubmit3}>
-                        <option value="N/A">Open Menu</option>
-                        <option value="000">Add to Favorites</option>
-                        <option value="000">LIKE video</option></select> */}
 
                     <Center>
                         <div>
@@ -105,7 +89,7 @@ class Videos extends Component {
                         </div>
                     </Center>
 
-                    <Button id='backtodash' bsStyle="primary" onClick={this.handleClick}>Back to Dashboard</Button>
+                    <Button style={{ color: blue900, fontWeight: "bold", fontSize: "large" }} bsStyle="primary" onClick={this.handleClick}>Back to Dashboard</Button>
                 </div>
             </ResponsiveContainer>
         );
