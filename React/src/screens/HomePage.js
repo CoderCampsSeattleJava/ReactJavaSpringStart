7c854e511a1e0f552def6537f942b330a58785a6
walaeJ <waljesusme2013@gmail.com>
2018-03-06 09:15:53 -0800

    Final Homepage

--- React/src/screens/HomePage.js
+++ React/src/screens/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import { NavLink } from 'react-router-dom';
-// import { Navbar, NavbarBrand, NavbarToggle, Collapse, NavItem, Nav } from "react-bootstrap";
+import { findDOMNode } from 'react-dom';
+import $ from 'jquery';
 
 class HomePage extends Component {
     render() {
@@ -12,7 +12,7 @@ class HomePage extends Component {
                             <i className="fa fa-bars fa-2x"></i>
                         </div>
                         <div className="logo">
-                            Coder Dudes
+                            Coder Dudes University
                         </div>
                         <div className="menu">
                             <ul>
@@ -22,30 +22,42 @@ class HomePage extends Component {
                             </ul>
                         </div>
                     </nav>
-
                     <div className="header">
                         <h2>Coder Dudes Are Awesome</h2>
-                        <h5>Life is so Great</h5>
-                        <p>Walae, Tyler, Nacho  </p>
+                        <h5>Life is so Great!</h5>
+                        <p>Walae, Tyler, Nacho</p>
                     </div>
-
                 </header>
                 <main className="text-center py-5">
                     <div className="container">
                         <div className="row">
                             <div className="col-md-30">
-                                <p align="justify">Some text will be writen </p>
+                                <p align="justify">Hello, If you want to understand how to program in Java properly, Guess what? you need to have a basic understanding of object orinted programing concepts and how they relate to Java programing.</p>
+                                <p>So trust us you will get the best and help full resource in Coder Dudes Favorite Page, then let's get in!</p>
                             </div>
                         </div>
                     </div>
                 </main>
             </div>
-
         );
     }
 }
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
+    });
+}
+);
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+    }
+    else {
+        $('nav').removeClass('black');
+    }
+}
+);
 
 
 
 export default HomePage;
-
