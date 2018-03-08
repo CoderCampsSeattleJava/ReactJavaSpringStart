727941b97d443386431a01e648a0d48e5686c3d8
Tyler Fuqua <tyler@agilityda.com>
2018-03-08 10:51:09 -0800

    finalcommit

--- React/src/screens/Dashboard.js
+++ React/src/screens/Dashboard.js
@@ -112,21 +112,7 @@ class Dashboard extends Component {
     );
   };
 };
-$(document).ready(function () {
-  $(".menu-icon").on("click", function () {
-      $("nav ul").toggleClass("showing");
-  });
-}
-);
-$(window).on("scroll", function () {
-  if ($(window).scrollTop()) {
-      $('nav').addClass('black');
-  }
-  else {
-      $('nav').removeClass('black');
-  }
-}
-);
+
   
 
 
